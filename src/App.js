d3c18a7d48e2124f9fb5887746e747ffee135678
kanizAlam <gsoftecprop@gmail.com>
2022-09-05 17:09:38 +0530

    Threejs_project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { BlendFunction } from "postprocessing";
+import './style.css';
+import { CubeCamera, Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { Ground } from './Ground';
+import { Car } from './Car';
+import { Rings } from './Ring';
+import { Boxes } from './Boxes';
+import { Bloom, ChromaticAberration, EffectComposer } from '@react-three/postprocessing';
+import { FloatingGrid } from './FloatingGrid';
+
+function CarShow() {
+  return (
+    <>
+      <OrbitControls target0={[0, 0.3, , 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+      <color args={[0, 0, 0]} attach='background' />
+      <CubeCamera resolution={256} frames= {Infinity}>
+        {(texture)=>(
+          <>
+          <Environment map={texture}/>
+          {/* <Car/> */}
+          </>
+        )}
+
+      </CubeCamera>
+     <Rings/>
+     <Boxes/>
+     <FloatingGrid/>
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <Ground/>
+      <EffectComposer>
+        <Bloom
+        blendFunction = {BlendFunction.ADD}
+        intensity = {1.3}
+        width={300}
+        height={300}
+        kernelSize = {5}
+        luminanceThreshold={0.65}
+        luminanceSmoothing = {0.025}
+        />
+
+        <ChromaticAberration 
+        blendFunction={BlendFunction.NORMAL}
+        offset = {[0.0005,0.0012]}
+        />
+      </EffectComposer>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Suspense fallback={null}>
+      <Canvas shadows>
+        <CarShow />
+      </Canvas>
+    </Suspense>
+  )
 }
 
 export default App;
